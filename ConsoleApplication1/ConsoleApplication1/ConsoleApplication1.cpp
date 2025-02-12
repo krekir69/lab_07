7968f37a57eae112b392a3c7d70f7a662d7b5ff4
krekir69 <kirillkretinin4216@gmail.com>
2025-02-12 22:39:18 +0700

    /

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d,n, f;
+	cin >> a;
+	b = 0;
+	c = 1;
+	f = 0;
+	d = 1;
+	n = 1;
+	while (n <= a)	{ 
+			f = b + c;
+			n = n + f;
+			b = c;
+			c = f ;
+			d = d + 1;
+		}
+	printf("Sum  %d  elem: %d \n", n,d);
+
+}
